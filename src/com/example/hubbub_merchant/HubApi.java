1c8653100bf3e483c23576c9f8bd7fe1c671b208
Ethan Welborn <welborn.ethan@gmail.com>
2014-02-12 00:38:44 -0700

    working on JSON request to hub

--- src/com/example/hubbub_merchant/HubApi.java
+++ src/com/example/hubbub_merchant/HubApi.java
@@ -1,34 +1,47 @@
 package com.example.hubbub_merchant;
 
+import java.io.InputStream;
+
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.protocol.HTTP;
 import org.json.JSONObject;
+
+import android.os.Looper;
 
 public class HubApi {
 	
-	public static HttpResponse write(JSONObject params) {
-		HttpClient httpClient = new DefaultHttpClient();
-		HttpResponse response;
-
-	    try {
-	        HttpPost request = new HttpPost("http://yoururl");
-	        request.addHeader("content-type", "application/x-www-form-urlencoded");
-	        response = httpClient.execute(request);
-	        
-	        return response;
-	    }catch (Exception ex) {
-	    	System.out.println(ex.getMessage().toString());
-	    } finally {
-	        httpClient.getConnectionManager().shutdown();
-	    }
-	    
-	    return null;
+	public static void write(JSONObject params) {
+		
+		HttpClient client = new DefaultHttpClient();
+        HttpConnectionParams.setConnectionTimeout(client.getParams(), 10000); //Timeout Limit
+        HttpResponse response;
+        Looper.prepare();
+        
+        HttpPost post = new HttpPost("http://hubbubapp.herokuapp.com/api/v1/interactions");
+        
+        try {
+        	StringEntity se = new StringEntity( params.toString() );
+            se.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+            post.setEntity(se);
+            response = client.execute(post);
+            
+            /*Checking response */
+            if(response!=null){
+                InputStream in = response.getEntity().getContent(); //Get the data in the entity
+            }
+        }
+        catch(Exception e) {
+        	e.printStackTrace();
+        }
+        
+        Looper.loop();
 		
 	}
 	
-	public static void read() {
-		
-	}
 }
